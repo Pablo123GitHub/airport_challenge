5527a81f4a6eff345c81dd706fc26b0d5a6fe176
Pablo <pabloandresvdl@gmail.com>
2017-10-03 21:41:33 +0100

    add land and take_off methods for the plane

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,5 +1,10 @@
 require './lib/airport.rb'
 
 class Plane
+def land(airport)
+end
+
+def take_off_from_airport(airport)
+end 
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,2 +1,15 @@
 require 'plane'
 require 'airport'
+
+describe Plane do
+    let(:airport) {Airport.new}
+  it 'expects the plane to land at an airport' do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+
+  it 'expects the plane to take_off from airport' do
+    expect(subject).to respond_to(:take_off_from_airport).with(1).argument
+  end
+
+
+end
